943ed10b0c5ac78465635e5394218b512d0bb75b
Juan Maldini <juan.maldini@outlook.com>
2024-09-16 23:31:27 +0200

    opcity hover

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const navBurguer = document.querySelector(".navBurguer");
 const navRight = document.querySelector(".navRight");
 
-// Mostrar el menú cuando haces clic en el ícono o cuando pasas el cursor sobre él.
-navBurguer.addEventListener("click", () => {
-    navRight.classList.add("active");
-});
-
+// Show the menu when hovering over the icon or the menu itself
 navBurguer.addEventListener("mouseenter", () => {
     navRight.classList.add("active");
+    navBurguer.classList.add("active");
 });
 
-// Ocultar el menú cuando el cursor se mueve fuera del menú (navRight).
+navRight.addEventListener("mouseenter", () => {
+    navRight.classList.add("active");
+    navBurguer.classList.add("active");
+});
+
+// Hide the menu when the mouse leaves the menu
 navRight.addEventListener("mouseleave", () => {
     navRight.classList.remove("active");
+    navBurguer.classList.remove("active");
 });
