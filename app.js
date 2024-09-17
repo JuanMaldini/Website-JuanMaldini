9f9f9712e32dd97cd2c567b70a6e45109b851329
Juan Maldini <juan.maldini@outlook.com>
2024-09-17 13:04:03 +0200

    up js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const navBurguer = document.querySelector(".navBurguer");
 const navRight = document.querySelector(".navRight");
 
-// Show the menu when hovering over the icon or the menu itself
+// Mostrar el menú cuando haces clic en el ícono.
 navBurguer.addEventListener("click", () => {
     navRight.classList.add("active");
     navBurguer.classList.add("active");
 });
 
-// Hide the menu when the mouse leaves the menu
+// Ocultar el menú cuando el cursor salga del menú.
 navRight.addEventListener("mouseleave", () => {
     navRight.classList.remove("active");
     navBurguer.classList.remove("active");
+});
+
+// Ocultar el menú cuando haces clic fuera de él.
+document.addEventListener("click", (event) => {
+    if (!navRight.contains(event.target) && !navBurguer.contains(event.target)) {
+        navRight.classList.remove("active");
+        navBurguer.classList.remove("active");
+    }
 });
